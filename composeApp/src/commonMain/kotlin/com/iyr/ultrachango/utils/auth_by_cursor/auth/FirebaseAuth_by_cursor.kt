fce476b750756d80c5545ec995a3301144b15c5a
romancanoniero <romancanoniero@gmail.com>
2025-03-05 22:30:48 -0300

    2025-03-05 - 10:26 PM

---
+++ composeApp/src/commonMain/kotlin/com/iyr/ultrachango/utils/auth_by_cursor/auth/FirebaseAuth_by_cursor.kt
@@ -0,0 +1,59 @@
+package com.iyr.ultrachango.utils.auth_by_cursor.auth
+
+expect class FirebaseAuth {
+    fun getCurrentUser(): NativeUser?
+    suspend fun signInWithEmailPassword(email: String, password: String): NativeAuthResult
+    suspend fun createUserWithEmailPassword(email: String, password: String): NativeAuthResult
+    suspend fun signOut()
+    fun addAuthStateListener(listener: (NativeUser?) -> Unit): AuthStateListener
+    fun removeAuthStateListener(listener: AuthStateListener)
+
+    // Phone Auth
+    suspend fun verifyPhoneNumber(phoneNumber: String): String
+    suspend fun signInWithPhoneCredential(verificationId: String, code: String): NativeAuthResult
+
+    // OAuth
+    suspend fun signInWithCredential(credential: AuthCredential): NativeAuthResult
+    suspend fun linkWithCredential(credential: AuthCredential): NativeAuthResult
+
+
+}
+
+expect class NativeUser {
+    val uid: String
+    val email: String?
+    val displayName: String?
+    val photoUrl: String?
+    val isEmailVerified: Boolean
+    val providerId: String
+}
+/*
+expect class NativeAuthResult {
+    val user: NativeUser?
+    val error: Exception?
+}
+*/
+expect class AuthCredential
+
+expect class AuthStateListener
+
+expect object GoogleAuthProvider {
+    fun getCredential(idToken: String, accessToken: String? = null): AuthCredential
+}
+
+expect object FacebookAuthProvider {
+    fun getCredential(accessToken: String): AuthCredential
+}
+
+expect object AppleAuthProvider {
+    fun getCredential(idToken: String, nonce: String? = null): AuthCredential
+}
+
+expect object TwitterAuthProvider {
+    fun getCredential(token: String, secret: String): AuthCredential
+}
+
+expect object PhoneAuthProvider {
+    fun getCredential(verificationId: String, code: String): AuthCredential
+}
+
